c58d4b1802e2c679464af1e6e0f93ef0b6d8189a
Paulo Santos <paulo_sss@hotmail.com>
2019-02-04 16:13:10 +0000

    passing Arrays and default Props

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "google"
 };
